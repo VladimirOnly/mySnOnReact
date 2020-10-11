f0041c01def5ad93d7c31426c06953d023e24b4e
VladimirOnly <plokhotnuik@gmail.com>
2020-10-11 19:36:56 +0200

    62 / 429 error

--- my-app/src/components/Users/Users.js
+++ my-app/src/components/Users/Users.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './users.module.css';
 import userImg from '../../data/img/userImg.png'
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 
 
@@ -34,12 +35,35 @@ let Users = (props) => {
                     <div>
                         {u.followed ?
                             <button onClick={() => {
-                                props.unfollow(u.id)
+
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "620e09cf-1b7f-46d8-a0e0-3377cab10dc7"
+                                    }
+                                })
+                                    .then(response => {
+                                        if(response.data.resultCode == 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    });
+
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
-                            }}>Follow</button>
-                        }
+
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}}`, {}, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "620e09cf-1b7f-46d8-a0e0-3377cab10dc7"
+                                    }
+                                })
+                                    .then(response => {
+                                        if(response.data.resultCode == 0) {
+                                        props.follow(u.id);
+                                        }
+                                    });
+
+                            }}>Follow</button>}
                     </div>
                 </span>
                 <span>
