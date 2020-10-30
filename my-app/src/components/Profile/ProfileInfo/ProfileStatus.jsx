bce43fe2ab8f9a69978ea223f89bc8c5e5aab75b
VladimirOnly <plokhotnuik@gmail.com>
2020-10-30 20:51:50 +0100

    72 update npm + unbind

--- my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -6,12 +6,12 @@ class ProfileStatus extends React.Component {
         editMode: false
     }
 
-    activateEditMode () {
+    activateEditMode = () => {
         this.setState({
             editMode: true
         })
     }
-    deactivateEditMode () {
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
         })
@@ -22,12 +22,12 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={ this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    <span onDoubleClick={ this.activateEditMode}>{this.props.status}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
                 </div>
                 }
             </div>
